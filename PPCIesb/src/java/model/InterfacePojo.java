f2c7505355ea46af00162250b481ab74c4b04ebc
Raquel Aires <31736228+QuelMaisQuel@users.noreply.github.com>
2018-05-22 20:39:10 -0300

    Add files via upload

---
+++ PPCIesb/src/java/model/InterfacePojo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//InterfacePojo.java
+package model;
+
+/**
+ *
+ * @author Raquel
+ */
+public interface InterfacePojo {
+    public int getCode();
+    public void setCode(int code);
+    public String getDescribe();
+    public void setDescribe(String describe);
+    
+}
